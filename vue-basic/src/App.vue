7c7d50c985a8e237931ad25d0ce53a8a6b8c66f6
ramset <setiawanramadyan@gmail.com>
2021-07-13 23:10:44 +0700

    v-for = looping

--- vue-basic/src/App.vue
+++ vue-basic/src/App.vue
@@ -1,30 +1,23 @@
 <template>
   <div>
-    <h1 v-if="status">ini tampil</h1>
-    <button @click="setStatus">toggle</button>
-    <p v-if="status">true</p>
-    <p v-else-if="status2">false</p>
-    <p v-else>yes</p>
+    <ol>
+      <li v-for="(todo, index) in todos" :key="todo.id">
+        {{ index }} - {{ todo.action }}
+      </li>
+    </ol>
   </div>
-
-  <!-- <div v-if="status">true</div>
-  <div v-else-if="status2">false</div>
-  <div v-else>yes</div> -->
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message: 'hi world',
-      status: false,
-      status2: true,
+      todos: [
+        { id: 1, action: 'wake up' },
+        { id: 2, action: 'learn' },
+        { id: 3, action: 'sleep' },
+      ],
     };
-  },
-  methods: {
-    setStatus() {
-      this.status = !this.status;
-    },
   },
 };
 </script>
